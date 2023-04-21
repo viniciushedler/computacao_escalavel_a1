85e09dac8ae334a8d720887a1cc99927c09412ba
Lf Laguardia <laguardialuis@gmail.com>
2023-04-21 19:14:11 -0300

    improvements world_creator

--- world_creator.py
+++ world_creator.py
@@ -3,7 +3,7 @@ import random
 if __name__ == "__main__":
     roads_number = 15
     lanes_number_range = [1, 3]
-    max_speed_range = [60, 120]
+    max_speed_range = [12, 24]
 
     with open("world.txt", "w") as f:
         initial_road = 100
@@ -15,6 +15,6 @@ if __name__ == "__main__":
             f.write(f"{left_lanes_number} {right_lanes_number}")
 
             max_speed = random.randint(max_speed_range[0], max_speed_range[1])
-            f.write(f" {str(max_speed).zfill(3)}")
+            f.write(f" {str(max_speed*5).zfill(3)}")
 
             f.write("\n")
