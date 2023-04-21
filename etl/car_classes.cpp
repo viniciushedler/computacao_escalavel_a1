7cd73534954111a68ed04737c8728f86c092b16a
Bruno Pereira Fornaro <brunopereirafornaro@gmail.com>
2023-04-21 11:35:23 -0300

    Cpplint

--- etl/car_classes.cpp
+++ etl/car_classes.cpp
@@ -1,11 +1,11 @@
-# ifndef CLASSES_CPP
-# define CLASSES_CPP
+#ifndef CLASSES_CPP
+#define CLASSES_CPP
 
+#include <array>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
-#include <array>
 
 using namespace std;
 
@@ -13,8 +13,8 @@ using namespace std;
 struct coords {
     int x;
     int y;
-    
-    coords() {};
+
+    coords(){};
 
     coords(int x, int y) {
         this->x = x;
@@ -28,98 +28,94 @@ struct coords {
 };
 
 class car {
-    public:
-        string plate;
-        coords position;
-        int speed;
-        int acceleration;
-        // ... outros atributos
+   public:
+    string plate;
+    coords position;
+    int speed;
+    int acceleration;
+    // ... outros atributos
 
-        car() {};
+    car(){};
 
-        car(string plate, coords position) {
-            this->plate = plate;
-            this->position = position;
-            this->speed = calculate_speed();
-            this->acceleration = calculate_acceleration();
-        };
+    car(string plate, coords position) {
+        this->plate = plate;
+        this->position = position;
+        this->speed = calculate_speed();
+        this->acceleration = calculate_acceleration();
+    };
 
-    private:
-        int calculate_speed() {
-            // ... calcula a velocidade
-            return 0;
-        };
+   private:
+    int calculate_speed() {
+        // ... calcula a velocidade
+        return 0;
+    };
 
-        int calculate_acceleration() {
-            // ... calcula a aceleração
-            return 0;
-        };
+    int calculate_acceleration() {
+        // ... calcula a aceleração
+        return 0;
+    };
 };
 
 class road {
-    public:
-        string name;
-        int length;
-        int width;
-        int speed_limit;
-        map<string, car> cars;
-        // ... outros atributos
+   public:
+    string name;
+    int length;
+    int width;
+    int speed_limit;
+    map<string, car> cars;
+    // ... outros atributos
 
-        road() {};
+    road(){};
 
-        road(int length, int width, int speed_limit) {
-            this->length = length;
-            this->width = width;
-            this->speed_limit = speed_limit;
-        };
+    road(int length, int width, int speed_limit) {
+        this->length = length;
+        this->width = width;
+        this->speed_limit = speed_limit;
+    };
 
-        bool has_car(string plate) {
-            if (cars.find(plate) == cars.end()) {
-                return false;
-            }
-            return true;
-        };
+    bool has_car(string plate) {
+        if (cars.find(plate) == cars.end()) {
+            return false;
+        }
+        return true;
+    };
 
-        car create_car(string plate, coords position) {
-            cars.insert(pair<string, car>(
-                plate,
-                car(plate, position)
-            ));
-            return cars.at(plate);
-        };
+    car create_car(string plate, coords position) {
+        cars.insert(pair<string, car>(plate, car(plate, position)));
+        return cars.at(plate);
+    };
 
-        car find_car(string plate) {
-            return cars.at(plate);
-        };
+    car find_car(string plate) { return cars.at(plate); };
 };
 
 class roads {
-    public:
-        map<string, road> roads_list;
+   public:
+    map<string, road> roads_list;
 
-        void update_car(string plate, coords position, string road_name) {
-            // define a estrada
-            if (roads_list.find(road_name) == roads_list.end()) {  // se a estrada não existe
-                roads_list.insert( pair<string, road>(
-                    road_name,
-                    road(10, 2, 60)  // <! Adicionar propriedades da estrada !>
+    void update_car(string plate, coords position, string road_name) {
+        // define a estrada
+        if (roads_list.find(road_name) ==
+            roads_list.end()) {  // se a estrada não existe
+            roads_list.insert(pair<string, road>(
+                road_name,
+                road(10, 2, 60)  // <! Adicionar propriedades da estrada !>
                 ));
-            }
-            road this_road = roads_list.at(road_name);
-
-            // define o carro
-            if (!this_road.has_car(plate)) {  // se o carro ainda não existe
-                this_road.create_car(plate, position);
-                return;
-            };
-            car this_car = this_road.find_car(plate); // se o carro já existe
-
-            /* <! Fazer cálculos de atualização do carro !>
-            aqui a gente vai mudar a posição dele,
-            tentar calcular velocidade e aceleração,
-            computar risco de colisão, essas coisas.
-            */
         }
+        road this_road = roads_list.at(road_name);
+
+        // define o carro
+        if (!this_road.has_car(plate)) {  // se o carro ainda não existe
+            this_road.create_car(plate, position);
+            return;
+        };
+        car this_car = this_road.find_car(plate);  // se o carro já existe
+
+        /* <! Fazer cálculos de atualização do carro !>
+        aqui a gente vai mudar a posição dele,
+        tentar calcular velocidade e aceleração,
+        computar risco de colisão, essas coisas.
+        */
+    }
 };
 
-# endif // CLASSES_CPP
+#endif  // CLASSES_CPP
--- etl/etl.cpp
+++ etl/etl.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
-#include <mutex>
+
 #include "car_classes.cpp"
 
 using namespace std;
@@ -19,16 +20,16 @@ void process_file_line() {
     string process_line;
     string process_road;
 
-    { // cria um escopo para mexer nas variáveis globais
-    lock_guard<mutex> guard(lines_queue_mutex);
-    process_line = lines_queue[doc_line];
-    doc_line++;
-    // se estivermos lendo uma linha de nova estrada
-    if (process_line.substr(0, 1) == ">") { 
-        road_name = process_line.substr(2, 8);
-        return;
-    }
-    process_road = road_name;
+    {  // cria um escopo para mexer nas variáveis globais
+        lock_guard<mutex> guard(lines_queue_mutex);
+        process_line = lines_queue[doc_line];
+        doc_line++;
+        // se estivermos lendo uma linha de nova estrada
+        if (process_line.substr(0, 1) == ">") {
+            road_name = process_line.substr(2, 8);
+            return;
+        }
+        process_road = road_name;
     }
 
     // faz o processamento aqui
@@ -37,7 +38,7 @@ void process_file_line() {
     roads_obj.update_car(car_plate, car_pos, process_road);
 }
 
-int read_f(string file_name){
+int read_f(string file_name) {
     ifstream file(file_name);
 
     if (!file.is_open()) {  // Verifica se o arquivo existe
@@ -47,7 +48,7 @@ int read_f(string file_name){
 
     string line;
     int lines_number = 0;
-    while(getline(file, line)) {
+    while (getline(file, line)) {
         lines_queue.push_back(line);
         lines_number++;
     }
@@ -57,7 +58,7 @@ int read_f(string file_name){
 int main() {
     int lines_number = read_f("carros.txt");
 
-    for (int i=0; i<lines_number; i++) {
+    for (int i = 0; i < lines_number; i++) {
         process_file_line();
     }
 }
