c7715ce762c69954b44035a54f933b4ba66733fc
VanessaWille <vanessa.b.wille@gmail.com>
2023-04-22 16:28:02 -0300

    testes dashboard

--- dashboard_test.cpp
+++ dashboard_test.cpp
@@ -13,19 +13,19 @@ int main() {
         std::cout << "\033[A\033[J\033[A\033[J\033[A\033[J";
         if (i % 3 == 0) {
             std::cout << "\033[31m";
-            cout << 1 << 1 << 1 << 1 << 1 << endl;
+            cout << 1 << 1 << "hghgkgkhkkjhkjhkj" << 1 << 1 << endl;
             std::cout << "\033[0m";
         }
         else {
-            cout << 1 << 1 << 1 << 1 << 1 << endl;
+            cout << 1 << 1 << "jhkjhkjhkjhkjhkjh" << 1 << 1 << endl;
         }
         std::cout << "Número de rodovias presentes na simulação: " << 10 - i << std::endl;
         std::cout << "Número total de veículos presentes na simulação: " << 10 - i << std::endl;
         std::cout << "Veículos acima do limite de velocidade: " << 10 - i << std::endl;
-        
+
         // std::cout << "Count: " << 10 - i << "\r";
         // std::cout.flush();
-        std::this_thread::sleep_for(std::chrono::milliseconds(6));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
         // system("cls");
     }
     return 0;
