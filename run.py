39a1cfad44d664654515a9ab2aa9ffd35ff763ab
viniciushedler <viniciushedler@gmail.com>
2023-04-23 18:53:54 -0300

    update on run.py

--- run.py
+++ run.py
@@ -10,3 +10,9 @@ if __name__=="__main__":
             os.system('cd etl')
             os.system('g++ etl.cpp -o etl')
             os.system('./etl')
+        case ('linux'):
+            os.system('python3 world_creator.py')
+            os.system('start cmd /k python3 mock/mock.py')
+            os.system('cd etl')
+            os.system('g++ etl.cpp -o etl')
+            os.system('./etl')
