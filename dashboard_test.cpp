a2c1e4047516506954c6ab5fcfd713a7707cc4f3
VanessaWille <vanessa.b.wille@gmail.com>
2023-04-22 23:15:31 -0300

    Mudanças dashboard

--- dashboard_test.cpp
+++ dashboard_test.cpp
@@ -1,43 +1,49 @@
-#ifndef DASHBOARD_TEST_CPP
-#define DASHBOARD_TEST_CPP
+// #ifndef DASHBOARD_TEST_CPP
+// #define DASHBOARD_TEST_CPP
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <chrono>
-#include <thread>
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
+// #include <string>
+// #include <vector>
+// #include <chrono>
+// #include <thread>
+// #ifdef _WIN32
+// #include <Windows.h>
+// #else
+// #include <unistd.h>
+// #endif
 
-using namespace std;
+// using namespace std;
 
+
+
+// int main() {
+//     for (int i = 0; i <300; i++) {
+//         std::cout << "\033[A\033[J\033[A\033[J\033[A\033[J";
+//         if (i % 3 == 0) {
+//             std::cout << "\033[31m";
+//             cout << 1 << 1 << "hghgkgkhkkjhkjhkj" << 1 << 1 << endl;
+//             std::cout << "\033[0m";
+//         }
+//         else {
+//             cout << 1 << 1 << "jhkjhkjhkjhkjhkjh" << 1 << 1 << endl;
+//         }
+//         std::cout << "Número de rodovias presentes na simulação: " << 10 - i << std::endl;
+//         std::cout << "Número total de veículos presentes na simulação: " << 10 - i << std::endl;
+//         std::cout << "Veículos acima do limite de velocidade: " << 10 - i << std::endl;
+
+//         // std::cout << "Count: " << 10 - i << "\r";
+//         // std::cout.flush();
+//         std::this_thread::sleep_for(std::chrono::milliseconds(10));
+//         sleep(0.1);
+//         // system("cls");
+//     }
+//     return 0;
+// };
+
+// #endif // DASHBOARD_TEST_CPP
 
 
 int main() {
-    for (int i = 0; i <300; i++) {
-        std::cout << "\033[A\033[J\033[A\033[J\033[A\033[J";
-        if (i % 3 == 0) {
-            std::cout << "\033[31m";
-            cout << 1 << 1 << "hghgkgkhkkjhkjhkj" << 1 << 1 << endl;
-            std::cout << "\033[0m";
-        }
-        else {
-            cout << 1 << 1 << "jhkjhkjhkjhkjhkjh" << 1 << 1 << endl;
-        }
-        std::cout << "Número de rodovias presentes na simulação: " << 10 - i << std::endl;
-        std::cout << "Número total de veículos presentes na simulação: " << 10 - i << std::endl;
-        std::cout << "Veículos acima do limite de velocidade: " << 10 - i << std::endl;
-
-        // std::cout << "Count: " << 10 - i << "\r";
-        // std::cout.flush();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        sleep(0.1);
-        // system("cls");
-    }
+    std::cout << "\033[38;5;208mHello, world!\033[0m" << std::endl;
     return 0;
-};
-
-#endif // DASHBOARD_TEST_CPP
+}
--- etl/dashboard_class.cpp
+++ etl/dashboard_class.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <chrono>
+
 
 #include "car_classes.cpp"
 
@@ -16,87 +18,106 @@ class dashboard{
     int number_cars_over_speed_limit;
     int number_cars_risk_collision;
     vector<car> all_cars_info;
-    // string plate;
-    // coords position;
-    // float speed;
-    // float acceleration;
-    // string propietary;
-    // string model;
-    // int year;
-    // int risk_collision;
 
     public:
-
         dashboard(roads *roads_obj) {
             this->number_roads = roads_obj->get_roads_count();
             this->number_cars = roads_obj->get_cars_count();
             this->number_cars_over_speed_limit = roads_obj->get_speeding_cars_count();
             this->number_cars_risk_collision = roads_obj->get_collision_risk_cars_count();
             this->all_cars_info = roads_obj->get_all_cars_info();
-            // this->plate = plate;
-            // this->GPS_position = GPS_position;
-            // this->speed = speed;
-            // this->acceleration = acceleration;
-            // this->risk_collision = risk_collision;
         };
 
+        // função para pegar os dados que serão printados
+        void get_data(int i, vector<car> all_cars_info){
+            if (all_cars_info[i].with_external_service_info == false){
+                cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
+                << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
+                << all_cars_info[i].acceleration << " " << all_cars_info[i].propietary << " " 
+                << all_cars_info[i].model << " " << all_cars_info[i].year << endl;
+            }
+            else{
+                cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
+                << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
+                << all_cars_info[i].acceleration << " " << endl;
+            }
+        };
+
+        // função para printar os dados 
         void print(){
-            std::cout << "\033[A\033[J\033[A\033[J\033[A\033[J";
-            // if (*find(cars_over_speed_limit.begin(), cars_over_speed_limit.end(), plate) == plate){
-            //     std::cout << "\033[31m";
-            //     cout << plate << GPS_position << speed << acceleration << risk_collision << endl;
-            //     std::cout << "\033[0m";
-            // }
-            // else{
-            //     cout << plate << GPS_position << speed << acceleration << risk_collision << endl;
-            // }
-
+            std::cout << "\033[A\033[J\033[A\033[J\033[A\033[J\033[A\033[J";
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
             // informations about all cars
-
-
-            for (int i = 0; i < all_cars_info.size(); i++){
-                if (all_cars_info[i].speed > 60){
-
-                    if (all_cars_info[i].propietary != ""){
-                        std::cout << "\033[31m";
-                        cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
-                        << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
-                        << all_cars_info[i].acceleration << " " << all_cars_info[i].propietary << " " 
-                        << all_cars_info[i].model << " " << all_cars_info[i].year << endl;
-                        std::cout << "\033[0m";
-                    }
-                    else{
-                        cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
-                        << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
-                        << all_cars_info[i].acceleration << " " << endl;
-                    }
+            for (int j = 0; j < all_cars_info.size(); j++){
+                if (all_cars_info[j].collision_status == 2){
+                    std::cout << "\033[31m";
+                    get_data(j, all_cars_info);
+                    std::cout << "\033[0m";
+                } else if (all_cars_info[j].collision_status == 1){
+                    std::cout << "\033[38;5;208m";
+                    get_data(j, all_cars_info);
+                    std::cout << "\033[0m";
+                } else if (all_cars_info[j].is_over_speed_limit == true){
+                    std::cout << "\033[33m";
+                    get_data(j, all_cars_info);
+                    std::cout << "\033[0m";
+                } else{
+                    get_data(j, all_cars_info);
                 }
+            }
+            // informations about roads
+            cout << "Number of roads: " << number_roads << endl;
+            cout << "Number of cars: " << number_cars << endl;
+            cout << "Number of cars over speed limit: " << number_cars_over_speed_limit << endl;
+            cout << "Number of cars at risk of collision: " << number_cars_risk_collision << endl;
+        };
+
+        // void print(){
+        //     dashboard dashboard_obj(&roads_obj);
+
+        //     std::cout << "\033[A\033[J\033[A\033[J\033[A\033[J\033[A\033[J";
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        //     // informations about all cars
+        //     for (int i = 0; i < all_cars_info.size(); i++){
+        //         if (all_cars_info[i].is_over_speed_limit == true){
+        //             std::cout << "\033[31m";
+        //             dashboard_obj.get_data(i, all_cars_info);
+        //             std::cout << "\033[0m";
+
+        //             // if (all_cars_info[i].with_external_service_info == false){
+        //             //     std::cout << "\033[31m";
+        //             //     cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
+        //             //     << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
+        //             //     << all_cars_info[i].acceleration << " " << all_cars_info[i].propietary << " " 
+        //             //     << all_cars_info[i].model << " " << all_cars_info[i].year << endl;
+        //             //     std::cout << "\033[0m";
+        //             // }
+        //             // else{
+        //             //     cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
+        //             //     << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
+        //             //     << all_cars_info[i].acceleration << " " << endl;
+        //             // }
+        //         }
                 
-                else{
-                    if (all_cars_info[i].propietary != ""){
-                        cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
-                        << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
-                        << all_cars_info[i].acceleration << " " << all_cars_info[i].propietary << " " 
-                        << all_cars_info[i].model << " " << all_cars_info[i].year << endl;
-                    }
-                    else{
-                        cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
-                        << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
-                        << all_cars_info[i].acceleration << " " << endl;
-                    }
-                }
-            };
-            cout << "Número de rodovias presentes na simulação: " << number_roads << endl;
-            cout << "Número total de veículos presentes na simulação: " << number_cars << endl;
-            cout << "Veículos acima do limite de velocidade: " << number_cars_over_speed_limit << endl;
-            cout << "Veículos em risco de colisão: " << number_cars_risk_collision << endl;
-            };
-
-        // void update(int number_roads, int number_cars, int number_cars_over_speed_limit){
-        //     this->number_roads = number_roads;
-        //     this->number_cars = number_cars;
-        //     this->number_cars_over_speed_limit = number_cars_over_speed_limit;
-        // };
+        //         else{
+        //             if (all_cars_info[i].propietary != ""){
+        //                 cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
+        //                 << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
+        //                 << all_cars_info[i].acceleration << " " << all_cars_info[i].propietary << " " 
+        //                 << all_cars_info[i].model << " " << all_cars_info[i].year << endl;
+        //             }
+        //             else{
+        //                 cout << all_cars_info[i].plate << " " << "(" << all_cars_info[i].position.x << ", " 
+        //                 << all_cars_info[i].position.y << ")" << " " << all_cars_info[i].speed << " " 
+        //                 << all_cars_info[i].acceleration << endl;
+        //             }
+        //         }
+        //     };
+        //     cout << "Número de rodovias presentes na simulação: " << number_roads << endl;
+        //     cout << "Número total de veículos presentes na simulação: " << number_cars << endl;
+        //     cout << "Veículos acima do limite de velocidade: " << number_cars_over_speed_limit << endl;
+        //     cout << "Veículos em risco de colisão: " << number_cars_risk_collision << endl;
+        //     };
 };
 
 #endif // DASHBOARD_CLASS_CPP
